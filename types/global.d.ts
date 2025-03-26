57d3b5ded284d1a65b1316a84b017b4511eefa83
Daniel Leonte <leontedaniel37@gmail.com>
2025-03-26 19:17:36 +0200

    types fix

--- types/global.d.ts
+++ types/global.d.ts
@@ -7,4 +7,16 @@ declare global {
 }
 
 // This export is needed to make this a module
-export {}; 
+export {};
+
+declare module 'webtorrent' {
+  // Use the declaration we created in webtorrent.d.ts
+}
+
+declare module 'ws' {
+  // Use the declaration we created in ws.d.ts
+}
+
+declare module 'express' {
+  // Use the declaration we created in express.d.ts
+} 
